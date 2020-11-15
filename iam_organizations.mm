b01bb7dd96c3b87ed66a3ad1f56a4ae4f61b4130
Fagner Candido <fsouzacandido@gmail.com>
2020-11-15 23:09:31 +0000

    added first part of iam mental mapping

---
+++ iam_organizations.mm
@@ -0,0 +1,78 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1605478198349" ID="ID_1755758782" MODIFIED="1605478364484" TEXT="AWS Organizations">
+<font NAME="Trebuchet MS" SIZE="16"/>
+<node CREATED="1605478389009" ID="ID_1289187290" MODIFIED="1605478500958" POSITION="right" TEXT="manage_account">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605478475869" ID="ID_301273947" MODIFIED="1605478500953" TEXT="add account">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605478484624" ID="ID_266333769" MODIFIED="1605478500958" TEXT="organize accounts">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605478519991" ID="ID_1324610528" MODIFIED="1605478538369" TEXT="create organizational units">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1605478730528" ID="ID_1768074978" MODIFIED="1605479891202" POSITION="left" TEXT="IAM">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605478743620" ID="ID_1415232355" MODIFIED="1605479891202" TEXT="authentication and authorization">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605478736531" ID="ID_41988086" MODIFIED="1605479891202" TEXT="free to use">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479608153" ID="ID_563407441" MODIFIED="1605479891202" TEXT="best practicles">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605479641366" ID="ID_1173350401" MODIFIED="1605479891201" TEXT="lock away your AWS account root user access keys">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479696143" ID="ID_1995027043" MODIFIED="1605479891201" TEXT="create individual IAM users">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479708257" ID="ID_166605496" MODIFIED="1605479891201" TEXT="use groups to assign permissions to IAM users">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479719488" ID="ID_121603760" MODIFIED="1605479891201" TEXT="grant least privilege">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479731280" ID="ID_955521369" MODIFIED="1605479891201" TEXT="get started using permissions with AWS managed policies">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479744659" ID="ID_695025570" MODIFIED="1605479891200" TEXT="use customer managed policies instead of inline policies">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479757555" ID="ID_322424150" MODIFIED="1605479891200" TEXT="use access levels to review IAM permissions">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479767634" ID="ID_779371766" MODIFIED="1605479891199" TEXT="configure a strong password policy for your users">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479774911" ID="ID_1354465316" MODIFIED="1605479891199" TEXT="enable MFA">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479797155" ID="ID_1269984945" MODIFIED="1605479891198" TEXT="use roles for applications that run on Amazon EC2 instances">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479806510" ID="ID_665132607" MODIFIED="1605479891197" TEXT="use roles to delegate permissions">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479815068" ID="ID_1210896462" MODIFIED="1605479891197" TEXT="do not share access keys">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479823724" ID="ID_334654234" MODIFIED="1605479891197" TEXT="rotate credentials regularly">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479832790" ID="ID_1248925942" MODIFIED="1605479891196" TEXT="remove unnecessary credentials">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479841555" ID="ID_782605810" MODIFIED="1605479891195" TEXT="use policy conditions for extra security">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479850130" ID="ID_868648607" MODIFIED="1605479891188" TEXT="monitor activity in your AWS account">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
