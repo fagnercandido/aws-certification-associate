6dd87462c53892d3cc29c3d063e6ab149ecd29ce
Fagner Candido <fsouzacandido@gmail.com>
2020-11-21 21:33:16 +0000

    added s3 and more features

--- iam_organizations.mm
+++ iam_organizations.mm
@@ -1,131 +1,264 @@
 <map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1605478198349" ID="ID_1755758782" MODIFIED="1605478364484" TEXT="AWS Organizations">
+<node CREATED="1605478198349" ID="ID_1755758782" MODIFIED="1605967938291" TEXT="AWS Organizations">
 <font NAME="Trebuchet MS" SIZE="16"/>
-<node CREATED="1605478389009" ID="ID_1289187290" MODIFIED="1605478500958" POSITION="right" TEXT="manage_account">
+<node CREATED="1605478389009" ID="ID_1289187290" MODIFIED="1605967938307" POSITION="right" TEXT="manage_account">
 <font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605478475869" ID="ID_301273947" MODIFIED="1605478500953" TEXT="add account">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605478484624" ID="ID_266333769" MODIFIED="1605478500958" TEXT="organize accounts">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605478519991" ID="ID_1324610528" MODIFIED="1605478538369" TEXT="create organizational units">
+<node CREATED="1605478475869" ID="ID_301273947" MODIFIED="1605967938307" TEXT="add account">
 <font NAME="Trebuchet MS" SIZE="14"/>
 </node>
-</node>
-</node>
-<node CREATED="1605478730528" ID="ID_1768074978" MODIFIED="1605479891202" POSITION="left" TEXT="IAM">
+<node CREATED="1605478484624" ID="ID_266333769" MODIFIED="1605967938307" TEXT="organize accounts">
 <font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605478743620" ID="ID_1415232355" MODIFIED="1605479891202" TEXT="authentication and authorization">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605478736531" ID="ID_41988086" MODIFIED="1605479891202" TEXT="free to use">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479608153" ID="ID_563407441" MODIFIED="1605479891202" TEXT="best practicles">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605479641366" ID="ID_1173350401" MODIFIED="1605479891201" TEXT="lock away your AWS account root user access keys">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479696143" ID="ID_1995027043" MODIFIED="1605479891201" TEXT="create individual IAM users">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479708257" ID="ID_166605496" MODIFIED="1605479891201" TEXT="use groups to assign permissions to IAM users">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479719488" ID="ID_121603760" MODIFIED="1605479891201" TEXT="grant least privilege">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479731280" ID="ID_955521369" MODIFIED="1605479891201" TEXT="get started using permissions with AWS managed policies">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479744659" ID="ID_695025570" MODIFIED="1605479891200" TEXT="use customer managed policies instead of inline policies">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479757555" ID="ID_322424150" MODIFIED="1605479891200" TEXT="use access levels to review IAM permissions">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479767634" ID="ID_779371766" MODIFIED="1605479891199" TEXT="configure a strong password policy for your users">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479774911" ID="ID_1354465316" MODIFIED="1605479891199" TEXT="enable MFA">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479797155" ID="ID_1269984945" MODIFIED="1605479891198" TEXT="use roles for applications that run on Amazon EC2 instances">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479806510" ID="ID_665132607" MODIFIED="1605479891197" TEXT="use roles to delegate permissions">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479815068" ID="ID_1210896462" MODIFIED="1605479891197" TEXT="do not share access keys">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479823724" ID="ID_334654234" MODIFIED="1605479891197" TEXT="rotate credentials regularly">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479832790" ID="ID_1248925942" MODIFIED="1605479891196" TEXT="remove unnecessary credentials">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479841555" ID="ID_782605810" MODIFIED="1605479891195" TEXT="use policy conditions for extra security">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605479850130" ID="ID_868648607" MODIFIED="1605479891188" TEXT="monitor activity in your AWS account">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-</node>
-<node CREATED="1605566052705" ID="ID_893468954" MODIFIED="1605568379007" TEXT="manage users">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605566060532" ID="ID_1776530814" MODIFIED="1605568379006" TEXT="add users and download credentials, needed">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-</node>
-<node CREATED="1605566207102" ID="ID_481313434" MODIFIED="1605568379007" TEXT="account setting">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605566214507" ID="ID_243006595" MODIFIED="1605568379001" TEXT="you can define policies to pass">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605566232324" ID="ID_10501277" MODIFIED="1605568379006" TEXT="least the characters">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605566254811" ID="ID_402773970" MODIFIED="1605568379006" TEXT="expiration time">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605566260153" ID="ID_1023051923" MODIFIED="1605568379006" TEXT="owasp rules to be simple">
+<node CREATED="1605478519991" ID="ID_1324610528" MODIFIED="1605967938306" TEXT="create organizational units">
 <font NAME="Trebuchet MS" SIZE="14"/>
 </node>
 </node>
 </node>
-<node CREATED="1605568358024" ID="ID_439742155" MODIFIED="1605568379007" TEXT="working with roles">
+<node CREATED="1605478730528" ID="ID_1768074978" MODIFIED="1605967970952" POSITION="left" TEXT="IAM">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605478743620" ID="ID_1415232355" MODIFIED="1605967970956" TEXT="authentication and authorization">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605478736531" ID="ID_41988086" MODIFIED="1605967970956" TEXT="free to use">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479608153" ID="ID_563407441" MODIFIED="1605967970956" TEXT="best practicles">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605479641366" ID="ID_1173350401" MODIFIED="1605967970956" TEXT="lock away your AWS account root user access keys">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605967036603" ID="ID_26928122" MODIFIED="1605967970956" TEXT="avoid use root user">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605967137238" ID="ID_1410375842" MODIFIED="1605967970956" TEXT="rotate access key">
 <font NAME="Trebuchet MS" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1605570087777" ID="ID_1992580340" MODIFIED="1605571085930" POSITION="right" TEXT="S3">
+<node CREATED="1605479696143" ID="ID_1995027043" MODIFIED="1605967970956" TEXT="create individual IAM users">
 <font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605570093607" ID="ID_1795913499" MODIFIED="1605571085933" TEXT="store objects">
+<node CREATED="1605967218290" ID="ID_1893457617" MODIFIED="1605967970956" TEXT="prefer this instead root user">
 <font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605570147775" ID="ID_573104578" MODIFIED="1605571085933" TEXT="keys">
+</node>
+</node>
+<node CREATED="1605479708257" ID="ID_166605496" MODIFIED="1605967970956" TEXT="use groups to assign permissions to IAM users">
 <font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605570153053" ID="ID_436814847" MODIFIED="1605571085933" TEXT="folder and file">
+</node>
+<node CREATED="1605479719488" ID="ID_121603760" MODIFIED="1605967970956" TEXT="grant least privilege">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479731280" ID="ID_955521369" MODIFIED="1605967970956" TEXT="get started using permissions with AWS managed policies">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479744659" ID="ID_695025570" MODIFIED="1605967970956" TEXT="use customer managed policies instead of inline policies">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479757555" ID="ID_322424150" MODIFIED="1605967970956" TEXT="use access levels to review IAM permissions">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479767634" ID="ID_779371766" MODIFIED="1605967970956" TEXT="configure a strong password policy for your users">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479774911" ID="ID_1354465316" MODIFIED="1605967970955" TEXT="enable MFA">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479797155" ID="ID_1269984945" MODIFIED="1605967970955" TEXT="use roles for applications that run on Amazon EC2 instances">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479806510" ID="ID_665132607" MODIFIED="1605967970955" TEXT="use roles to delegate permissions">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479815068" ID="ID_1210896462" MODIFIED="1605967970955" TEXT="do not share access keys">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479823724" ID="ID_334654234" MODIFIED="1605967970955" TEXT="rotate credentials regularly">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479832790" ID="ID_1248925942" MODIFIED="1605967970955" TEXT="remove unnecessary credentials">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479841555" ID="ID_782605810" MODIFIED="1605967970955" TEXT="use policy conditions for extra security">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605479850130" ID="ID_868648607" MODIFIED="1605967970955" TEXT="monitor activity in your AWS account">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605566052705" ID="ID_893468954" MODIFIED="1605967970955" TEXT="manage users">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605566060532" ID="ID_1776530814" MODIFIED="1605967970955" TEXT="add users and download credentials, needed">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605566207102" ID="ID_481313434" MODIFIED="1605967970955" TEXT="account setting">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605566214507" ID="ID_243006595" MODIFIED="1605967970955" TEXT="you can define policies to pass">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605566232324" ID="ID_10501277" MODIFIED="1605967970955" TEXT="least the characters">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605566254811" ID="ID_402773970" MODIFIED="1605967970955" TEXT="expiration time">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605566260153" ID="ID_1023051923" MODIFIED="1605967970955" TEXT="owasp rules to be simple">
 <font NAME="Trebuchet MS" SIZE="14"/>
 </node>
 </node>
 </node>
-<node CREATED="1605570100716" ID="ID_121754340" MODIFIED="1605571085933" TEXT="store objects in buckets">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605570184239" ID="ID_186047265" MODIFIED="1605571085933" TEXT="regional with vision global">
-<font NAME="Trebuchet MS" SIZE="14"/>
-</node>
-<node CREATED="1605570264676" ID="ID_1649216605" MODIFIED="1605571085933" TEXT="class">
-<font NAME="Trebuchet MS" SIZE="14"/>
-<node CREATED="1605570268406" ID="ID_1201532153" MODIFIED="1605571085933" TEXT="the price change">
+<node CREATED="1605568358024" ID="ID_439742155" MODIFIED="1605967970955" TEXT="working with roles">
 <font NAME="Trebuchet MS" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1605571073592" ID="ID_1343102932" MODIFIED="1605571085933" TEXT="blocked all public access">
-<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1605570087777" ID="ID_1992580340" MODIFIED="1605993800062" POSITION="right" TEXT="S3">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605570093607" ID="ID_1795913499" MODIFIED="1605993800076" TEXT="store objects">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605570147775" ID="ID_573104578" MODIFIED="1605993800076" TEXT="keys">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605570153053" ID="ID_436814847" MODIFIED="1605993800075" TEXT="folder and file">
+<font NAME="Trebuchet MS" SIZE="14"/>
 </node>
-<node CREATED="1605571446491" ID="ID_1149765095" MODIFIED="1605571453133" TEXT="policies in json format"/>
-<node CREATED="1605571454392" ID="ID_1715325354" MODIFIED="1605571474856" TEXT="to resource be public the bucket must be public"/>
+</node>
+</node>
+<node CREATED="1605570100716" ID="ID_121754340" MODIFIED="1605993800075" TEXT="store objects in buckets">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605570184239" ID="ID_186047265" MODIFIED="1605993800075" TEXT="regional with vision global">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605570264676" ID="ID_1649216605" MODIFIED="1605993800075" TEXT="class">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605570268406" ID="ID_1201532153" MODIFIED="1605993800075" TEXT="the price change">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605571073592" ID="ID_1343102932" MODIFIED="1605993800075" TEXT="blocked all public access">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605571446491" ID="ID_1149765095" MODIFIED="1605993800075" TEXT="policies in json format">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605571454392" ID="ID_1715325354" MODIFIED="1605993800075" TEXT="to resource be public the bucket must be public">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963050790" ID="ID_1342243353" MODIFIED="1605993800074" TEXT="security">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605963057854" ID="ID_1764023511" MODIFIED="1605993800074" TEXT="HTTPS">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963106007" ID="ID_1701841096" MODIFIED="1605993800074" TEXT="encryption">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605963123867" ID="ID_937704481" MODIFIED="1605993800074" TEXT="server-side">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963132094" ID="ID_945628148" MODIFIED="1605993800074" TEXT="client-side">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605963203442" ID="ID_471145631" MODIFIED="1605993800074" TEXT="of the cloud - aws">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963214202" ID="ID_391007570" MODIFIED="1605993800074" TEXT="in the cloud - client">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605963494246" ID="ID_525030906" MODIFIED="1605993800074" TEXT="versioning">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605963500620" ID="ID_1052478708" MODIFIED="1605993800073" TEXT="3 states">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605963506473" ID="ID_309242599" MODIFIED="1605993800073" TEXT="not enabled">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963512506" ID="ID_1666860453" MODIFIED="1605993800073" TEXT="enabled">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963514940" ID="ID_1943241385" MODIFIED="1605993800073" TEXT="suspended">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605963526914" ID="ID_1993409862" MODIFIED="1605993800073" TEXT="once enabled not disabled">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963535849" ID="ID_962695980" MODIFIED="1605993800073" TEXT="each objects has one id">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963578805" ID="ID_1651105724" MODIFIED="1605993800073" TEXT="enable mfa to delete objects">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963822965" ID="ID_210163763" MODIFIED="1605993800073" TEXT="authorize linking with id">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605963837261" ID="ID_50224824" MODIFIED="1605993800072" TEXT="if enable the new file, but different id, the new object is not available by default">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605963993190" ID="ID_655549750" MODIFIED="1605993800072" TEXT="lifecycle">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605963997468" ID="ID_412040290" MODIFIED="1605993800072" TEXT="transition action">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605964012163" ID="ID_1421932019" MODIFIED="1605993800072" TEXT="expiration action">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605964455071" ID="ID_10642508" MODIFIED="1605993800072" TEXT="replication">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605964459228" ID="ID_560850572" MODIFIED="1605993800072" TEXT="not async">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605964489623" ID="ID_253009315" MODIFIED="1605993800072" TEXT="CRR - between regions">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605964516070" ID="ID_1837040173" MODIFIED="1605993800071" TEXT="SRR - same region">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605964738015" ID="ID_413688265" MODIFIED="1605993800071" TEXT="cross bucket">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1605964745347" ID="ID_1406225017" MODIFIED="1605993800071" TEXT="it&apos;s possible">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605967912155" ID="ID_133011239" MODIFIED="1605993800071" TEXT="simple storage service">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1605989591284" ID="ID_427507906" MODIFIED="1605993800071" TEXT="store and retrieve">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605990891055" ID="ID_183068789" MODIFIED="1605993800071" TEXT="up 5TB size">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605990943532" ID="ID_1943035997" MODIFIED="1605993800071" TEXT="rest or soap interfaces">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1605990984684" ID="ID_1438435178" MODIFIED="1605993800070" TEXT="soap">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1605990988506" ID="ID_1745098752" MODIFIED="1605993800070" TEXT="http is deprecated">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605990994845" ID="ID_1224729992" MODIFIED="1605993800070" TEXT="https is enabled">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605991029346" ID="ID_87752423" MODIFIED="1605993800070" TEXT="not supported in the future">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1605991140076" ID="ID_1272594213" MODIFIED="1605993800070" TEXT="buckets">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1605991144460" ID="ID_1029178427" MODIFIED="1605993800070" TEXT="namespaces">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605991202603" ID="ID_866456883" MODIFIED="1605993800070" TEXT="created in AWS region">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605992999438" ID="ID_514296872" MODIFIED="1605993800069" TEXT="name is globally unique">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1605993170251" ID="ID_1142220294" MODIFIED="1605993800069" TEXT="buckets near use">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1605993774143" ID="ID_877590053" MODIFIED="1605993800068" TEXT="you can use bittorrent">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 </map>
