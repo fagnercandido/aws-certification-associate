b05afaae5bd94ddcbef674135a709ae3f0352cb5
Fagner Candido <fsouzacandido@gmail.com>
2020-11-22 21:39:30 +0000

    added mental mind to ec2 and other services

---
+++ ec2.mm
@@ -0,0 +1,168 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1605996364392" ID="ID_189055192" MODIFIED="1606080969296" TEXT="EC2">
+<font NAME="Trebuchet MS" SIZE="16"/>
+<node CREATED="1606047162183" ID="ID_830699694" MODIFIED="1606080963366" POSITION="left" TEXT="storage, not ec2 yet">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046530737" ID="ID_715923895" MODIFIED="1606080963366" TEXT="storage gateway">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046541020" ID="ID_1188039394" MODIFIED="1606080963366" TEXT="file gateway">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606046546831" ID="ID_681590039" MODIFIED="1606080963366" TEXT="volume gateway">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046747071" ID="ID_825684044" MODIFIED="1606080963366" TEXT="cache">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606046751269" ID="ID_747005358" MODIFIED="1606080963366" TEXT="stored">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046783592" ID="ID_1728558466" MODIFIED="1606080963366" TEXT="without ec2">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1606046556984" ID="ID_1445238345" MODIFIED="1606080963366" TEXT="tape gateway">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606046212447" ID="ID_1318823401" MODIFIED="1606080963366" TEXT="snowball">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046219562" ID="ID_563475268" MODIFIED="1606080963366" TEXT="data migrate">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606046272937" ID="ID_690344235" MODIFIED="1606080963366" TEXT="more than 10TB">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046281522" ID="ID_1004368789" MODIFIED="1606080963366" TEXT="economic fact">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606046311250" ID="ID_409996013" MODIFIED="1606080963366" TEXT="large data">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606046341886" ID="ID_303245927" MODIFIED="1606080963366" TEXT="not work out of USA">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606046896275" ID="ID_219981210" MODIFIED="1606080963366" TEXT="cloud front">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046901041" ID="ID_1187492567" MODIFIED="1606080963366" TEXT="cdn of aws">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606046914378" ID="ID_849446318" MODIFIED="1606080963366" TEXT="improve access your content">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606046936080" ID="ID_928175640" MODIFIED="1606080963366" TEXT="static and dynamic content">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606047111921" ID="ID_194179376" MODIFIED="1606080963366" TEXT="associated with edge locations">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+</node>
+<node CREATED="1606047199648" ID="ID_595722988" MODIFIED="1606080963366" POSITION="right" TEXT="amazon elastic compute cloud">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606047274346" ID="ID_687137628" MODIFIED="1606080963365" POSITION="right" TEXT="pricing">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606047277263" ID="ID_1278596217" MODIFIED="1606080963365" TEXT="on demand">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606047307149" ID="ID_1796671301" MODIFIED="1606080963365" TEXT="spot instance">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606047313054" ID="ID_108967735" MODIFIED="1606080963365" TEXT="90% better than on demand, in pricing">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606047361874" ID="ID_1951416919" MODIFIED="1606080963365" TEXT="saving plans">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606047366711" ID="ID_1980250813" MODIFIED="1606080963365" TEXT="1 until 3 yeas">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606047383814" ID="ID_1001864559" MODIFIED="1606080963364" TEXT="reserved instance">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606047392399" ID="ID_63090670" MODIFIED="1606080963364" TEXT="1 until 3 years">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606047403090" ID="ID_683585498" MODIFIED="1606080963364" TEXT="dedicated host">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606047524934" ID="ID_1870429958" MODIFIED="1606080963364" POSITION="left" TEXT="ami">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606047529738" ID="ID_1790502396" MODIFIED="1606080963364" TEXT="images">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606047764459" ID="ID_402149324" MODIFIED="1606080963364" POSITION="right" TEXT="provide machines">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606050832865" ID="ID_79280561" MODIFIED="1606080963364" POSITION="left" TEXT="ssh access">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606050836427" ID="ID_784089475" MODIFIED="1606080963364" TEXT="chmod 400 *.pem">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606052490711" ID="ID_1157739041" MODIFIED="1606080963364" POSITION="right" TEXT="security group">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606052496255" ID="ID_243591190" MODIFIED="1606080963363" TEXT="always permissive">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606052508360" ID="ID_1252360275" MODIFIED="1606080963363" TEXT="by default all traffic outbound is free">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606052552812" ID="ID_1327206578" MODIFIED="1606080963363" TEXT="are stateful">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606067199236" ID="ID_297735270" MODIFIED="1606080963363" POSITION="left" TEXT="EBS">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606067205433" ID="ID_1273359251" MODIFIED="1606080963363" TEXT="disk in a instance">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606067220177" ID="ID_28834506" MODIFIED="1606080963363" TEXT="amazon elastic block store">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606080006810" ID="ID_410747289" MODIFIED="1606080963363" POSITION="right" TEXT="elastic network interface">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606080026689" ID="ID_52672828" MODIFIED="1606080963363" TEXT="eni">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606080029442" ID="ID_1997079075" MODIFIED="1606080963363" TEXT="efa">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606080036742" ID="ID_496500446" MODIFIED="1606080963363" TEXT="ena">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606080144608" ID="ID_1707308296" MODIFIED="1606080963363" POSITION="left" TEXT="efs">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606080147873" ID="ID_508491313" MODIFIED="1606080963363" TEXT="file system based in nfs">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606080770868" ID="ID_460963807" MODIFIED="1606080963362" TEXT="not compatible with windows">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606080792458" ID="ID_1493620140" MODIFIED="1606080963362" TEXT="amazon fsX para Windows">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606080839142" ID="ID_1638504526" MODIFIED="1606080963362" TEXT="lustre to hpc">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+<node CREATED="1606080878738" ID="ID_658660710" MODIFIED="1606080963362" POSITION="right" TEXT="placement groups">
+<font NAME="Trebuchet MS" SIZE="14"/>
+<node CREATED="1606080924889" ID="ID_1785144589" MODIFIED="1606080963362" TEXT="cluster">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606080927832" ID="ID_178513325" MODIFIED="1606080963362" TEXT="partition">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+<node CREATED="1606080941851" ID="ID_253427215" MODIFIED="1606080963362" TEXT="spread">
+<font NAME="Trebuchet MS" SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
